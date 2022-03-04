2e34b0061408b92534b660dc32535302914f90d8
paulbradfo <90589413+paulbradfo@users.noreply.github.com>
2022-03-04 16:23:26 -0700

    Assignment #9

---
+++ book/Components/CartSummaryViewComponent.cs
@@ -0,0 +1,24 @@
+﻿using book.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace book.Components
+{
+    public class CartSummaryViewComponent : ViewComponent
+    {
+        private ShopCart cart;
+
+        public CartSummaryViewComponent(ShopCart cartService)
+        {
+            cart = cartService;
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            return View(cart);
+        }
+    }
+}
